cc41ad70f33cf773ec5f401988f23491fb9dbd05
Vinícius Pires Chaves <106564445+vpireschaves@users.noreply.github.com>
2025-05-18 19:01:21 -0300

    fix settings.py

--- projeto_integrador/settings.py
+++ projeto_integrador/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#hi2qi+x=#u0uc!1041eex_!2@&vm@oa0n=vlqd1z=6zzw4+9y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
